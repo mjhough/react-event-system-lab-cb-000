2d98fe36e60cdbeef278978a0749d1790ab471fe
Matt Hough <matt@hough.tv>
2018-02-08 07:30:51 +1000

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+  goodLog = () => (console.log('Good!'));
+  eyesOnLog = () => (console.log('Hey! Eyes on me!'));
+
+  render() {
+    return (
+      <button onFocus={this.goodLog} onBlur={this.eyesOnLog}>BUTTON</button>
+    );
+  }
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
-// Code Keypad Component Here
+import React from 'react';
+
+class Keypad extends React.Component {
+  logIt = () => (console.log('Entering password...'));
+  
+  render() {
+    return (
+      <input type="password" name="password" onKeyUp={this.logIt} />
+    )
+  }
+}
+
+export default Keypad;
